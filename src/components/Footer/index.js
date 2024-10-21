ddca9f45d43d7457822d60345531b7010a0bb53d
shreeji <solutionkmitjaydip@gmail.com>
2024-10-21 19:17:05 +0530

    Feat: code refactor

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,36 @@ import Button from "../common/Button";
 import { FaCircleChevronLeft } from "react-icons/fa6";
 
 function Footer() {
+  const sections = [
+    {
+      title: "Organizations",
+      links: [
+        "Certifications",
+        "Best Practices",
+        "Social",
+        "Directory",
+        "Build Your Dream",
+      ],
+    },
+    {
+      title: "Individuals",
+      links: [
+        "Social",
+        "Build Your Dream",
+        "Directory",
+        "Marketplace",
+        "Best Practices",
+      ],
+    },
+    {
+      title: "Company",
+      links: ["About Us", "Careers", "Press", "News", "Contact"],
+    },
+    {
+      title: "Legal",
+      links: ["Terms", "Privacy", "Cookies", "Licenses", "Settings"],
+    },
+  ];
   return (
     <footer className="bg-bgGray">
       <div className="container px-4 lg:px-6 py-2.5">
@@ -11,8 +41,8 @@ function Footer() {
           <div>
             <h3 className="text-mainColor">Be Connected. Be Conscious</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
+              Get the latest updates, insights, and stories from our conscious
+              community <br /> delivered straight to your inbox.
             </p>
           </div>
           <div className="flex items-center gap-[10px]">
@@ -31,148 +61,36 @@ function Footer() {
           </div>
         </div>
         <div className="pt-[30px] md:gap-[40px] md:flex justify-between">
-          <div className="mb-6 md:mb-0 max-w-[350px]">
+          <div className="mb-6 md:mb-0 max-w-[400px]">
             <Link to="/" className="flex items-center">
               <span className="self-center text-xl font-semibold text-mainColor">
                 logo
               </span>
             </Link>
             <p className="mt-[20px]">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book
+              Embark on a journey with CNESS, where every step you take is
+              towards a more conscious and connected world. Here, your dreams
+              find wings, your ideas find companions, and your actions inspire
+              change.
             </p>
           </div>
           <div className="w-full grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-mainColor">
-                Organizations
-              </h2>
-              <ul className="">
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Certifications
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Best Practices
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Social
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Directory
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Build Your Dream
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-mainColor">
-                Organizations
-              </h2>
-              <ul className="">
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Certifications
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Best Practices
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Social
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Directory
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Build Your Dream
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-mainColor">
-                Organizations
-              </h2>
-              <ul className="">
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Certifications
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Best Practices
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Social
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Directory
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Build Your Dream
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-mainColor">
-                Organizations
-              </h2>
-              <ul className="">
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Certifications
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Best Practices
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Social
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Directory
-                  </Link>
-                </li>
-                <li className="mb-4">
-                  <Link to="" className="hover:underline">
-                    Build Your Dream
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            {sections.map(({ title, links }, idx) => (
+              <div key={idx}>
+                <h2 className="mb-6 text-sm font-semibold text-mainColor">
+                  {title}
+                </h2>
+                <ul>
+                  {links.map((link, i) => (
+                    <li key={i} className="mb-4">
+                      <Link to="" className="hover:underline">
+                        {link}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
